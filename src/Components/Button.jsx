26e0aba754d65fec1539345dbb69d358bd895806
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2024-05-09 19:08:55 +0530

    error button explore

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 function Button({ children, disabled, to, type, onClick }) {
   const base =
     "inline-block text-sm px-4 text-gray-900 py-2 md:px-6 md:py-4 rounded-full bg-stone-100 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-slate-900 hover:text-white focus:bg-slate-500 focus:outline-none focus:ring focus:ring-slate-800 focus:ring-offset-2 disabled:cursor-not-allowed focus:text-slate-50";
-  const submitbutton =
-    "inline-block w-[417px] h-[75px] text-sm px-4 text-gray-900 py-2 md:px-4 md:py-2 rounded-[65.22px] bg-[#C9C9C9] font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-slate-900 hover:text-white focus:bg-slate-500 focus:outline-none focus:ring focus:ring-slate-800 focus:ring-offset-2 disabled:cursor-not-allowed focus:text-slate-50";
+
   if (to)
     return (
       <Link to={to} className={base}>
@@ -20,7 +19,7 @@ function Button({ children, disabled, to, type, onClick }) {
     );
 
   return (
-    <button disabled={disabled} className={submitbutton}>
+    <button disabled={disabled} className={base}>
       {children}
     </button>
   );
